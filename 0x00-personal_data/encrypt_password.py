c22abaa7a538832dcb061cbfc6fd7dec726d2d28
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-31 01:58:32 +0300

    check valid password using bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,10 @@ def hash_password(password: str) -> bytes:
     Salts and hashes a password.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
