eb5cfec2866a254eb1b2b608fc0c442c595da12d
Evance Odoyo <odoyoevanc@gmail.com>
2024-06-04 16:16:47 +0300

    method to return user object from with given credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,9 @@
 """Module for basic authentication.
 """
 import base64
+from typing import TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -46,7 +48,8 @@ class BasicAuth(Auth):
         self,
         decoded_base64_authorization_header: str
     ) -> (str, str):
-        """Returns user email and password from Base64 decoded value as a tuple.
+        """Returns user email and password from Base64 decoded
+        value as a tuple.
         """
         if (
             decoded_base64_authorization_header is None or
@@ -56,3 +59,22 @@ class BasicAuth(Auth):
             return (None, None)
 
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(
+        self,
+        user_email: str,
+        user_pwd: str
+    ) -> TypeVar('User'):
+        """Returns `User` instance based on his email and password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.search({"email": user_email})
+        if not users:
+            return None
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
