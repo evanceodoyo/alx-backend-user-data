b3657caabb3d90c2bc0e9c6ccc0d4497498cfb64
Evance Odoyo <odoyoevanc@gmail.com>
2024-06-07 23:14:10 +0300

    finish task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -45,11 +45,12 @@ def before_req():
     ):
         return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
 
     if auth.session_cookie(request) is None:
-        abort(401)
+        abort(403)
 
     if auth.current_user(request) is None:
         abort(403)
