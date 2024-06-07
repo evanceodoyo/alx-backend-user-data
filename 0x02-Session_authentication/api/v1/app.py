4cb59439354787503049fb1e511e88350e7c0cfa
Evance Odoyo <odoyoevanc@gmail.com>
2024-06-07 21:35:47 +0300

    test on checker

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -45,12 +45,11 @@ def before_req():
     ):
         return
 
-    if (auth.authorization_header(request) is None
-            and auth.session_cookie(request) is None):
-        abort(401)
-
     if auth.authorization_header(request) is None:
-        abort(401)
+        abort(403)
+
+    if auth.session_cookie(request) is None:
+        abort(403)
 
     if auth.current_user(request) is None:
         abort(403)
