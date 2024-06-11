6751e18eeff576eb8e4dc43e0b67e0359094a450
Evance Odoyo <odoyoevanc@gmail.com>
2024-06-11 18:50:44 +0300

    method to persist usser to the db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,5 +39,5 @@ class DB:
           User object
         """
         user = User(email=email, hashed_password=hashed_password)
-        self.__session.add(user)
+        self._session.add(user)
         return user
