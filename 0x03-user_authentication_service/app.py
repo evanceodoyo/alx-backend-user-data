2bc42b6f73e0e4a92b808475b73ef0b04e7d41f6
Evance Odoyo <odoyoevanc@gmail.com>
2024-06-12 02:26:53 +0300

    implement logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app
 """
-from flask import abort, Flask, jsonify, request
+from flask import abort, Flask, jsonify, request, redirect
 from auth import Auth
 
 
@@ -52,6 +52,20 @@ def login() -> str:
     res = jsonify({"email": email, "message": "logged in"})
     res.set_cookie("session_id", session_id)
     return res
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """DELETE /sessions
+    Returns:
+      Redirect to home route (/)
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destory_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
